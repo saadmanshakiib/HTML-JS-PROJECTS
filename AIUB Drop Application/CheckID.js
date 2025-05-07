d68bf8ca6d7e28ade91e1f59b2ab748bb9486b72
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2025-05-07 08:10:46 +0600

    Add files via upload

--- AIUB Drop Application/CheckID.js
+++ AIUB Drop Application/CheckID.js
@@ -1,9 +1,18 @@
 function CheckID() {
-    const ID = document.getElementById("varsity_pass").value;
-    if(ID == 53467){
-        window.location.href="drop_form.html";
+    const ID = document.getElementById("ID").value;
+    const name = document.getElementById("name").value;
+    const semester = document.getElementById("semester_check").value;
+
+    if(ID == "" || name ==""){
+        alert("Please insert both user name & student id ");
+       }
+       else{
+        if(semester == 1)window.location.href="1stSem.html";
+        else if(semester == 2) window.location.href="2ndSem.html";
+        else if(semester == 3) window.location.href="3rdSem.html";
+        else if(semester == 4) window.location.href="4thSem.html";
+        else if(semester == 5) window.location.href="5thSem.html";
+        else if(semester == 6) window.location.href="6thSem.html";
+       }
+
     }
-    else if(ID != 53467){
-        alert("Invalid Password");
-    }
-}
